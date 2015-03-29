57e0e4b20f8de77a389bf9a6361e43c32a92d5a9
Gyuwon <gyuwon@live.com>
2015-03-29 21:10:28 +0900

    added ViewModel

---
+++ UserManager/UserManager/ViewModels/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UserManager.ViewModels
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler.Invoke(this, e);
+            }
+        }
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T field,
+                                      T value,
+                                      [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+            {
+                return false;
+            }
+
+            field = value;
+            NotifyPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
