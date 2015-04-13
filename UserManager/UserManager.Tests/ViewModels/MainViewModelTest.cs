4d3979bb1e438a35261cc4e396f335704545aff8
Gyuwon Yi <gyuwon@tidcorp.com>
2015-04-13 22:15:35 +0900

    NameEntryShouldBeClearedWhenAddCommandExecuted() 테스트가 실패합니다.

---
+++ UserManager/UserManager.Tests/ViewModels/MainViewModelTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserManager.ViewModels;
+
+namespace UserManager.Tests.ViewModels
+{
+    [TestClass]
+    public class MainViewModelTest
+    {
+        [TestMethod]
+        public void NameEntryShouldBeClearedWhenAddCommandExecuted()
+        {
+            // Arrange
+            var viewModel = new MainViewModel();
+            viewModel.NameEntry = new Random().Next().ToString();
+
+            // Act
+            viewModel.Add.Execute(parameter: null);
+
+            // Assert
+            viewModel.NameEntry.Should().BeNullOrEmpty();
+        }
+    }
+}
